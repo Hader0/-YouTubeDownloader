db3c835801ee936a43be9c72c2e83d13259801ab
Hayden <108206953+Hader0@users.noreply.github.com>
2022-09-06 17:39:19 +1000

    Update main.py

--- YouTubeToMp4/main.py
+++ YouTubeToMp4/main.py
@@ -4,23 +4,31 @@ from pytube import YouTube
 from tkinter import filedialog
 
 # Function 
-def downloadFunc():
-    url = YouTubelink.get())
-    print(url.title(（
-
 
 def Location():
+    global dirText
     locationLabel = Label(root)
     locationLabel.grid(row=5, column=2, pady=(20, 0))
     locationlol = filedialog.askdirectory()
-    locationLabel.insert(END, locationlol)
+    dirText = str(locationlol)
+    locationLabel.config(text=str(locationlol))
+    return dirText
+
+def downloadFunc():
+    url = YouTube(link.get())
+    urlDown = url.streams.get_highest_resolution()
+    urlDown.download(str(dirText))
+    print(str(dirText))
 
 
 root = Tk()
-root.geometry('500x300') 
+root.geometry('500x350') 
 root.title('Youtube Video/Music Downloader')
 root.resizable(False, False)
 
+root.columnconfigure(0, weight=0)
+root.columnconfigure(1, weight=0)
+root.columnconfigure(2, weight=1)
 
 link = StringVar()
 
@@ -29,17 +37,17 @@ title.grid(row=0, column=2, pady=(20,50))
 
 labelText = Label(root, text="Paste Link:", font=('Helvetica', 15))
 labelText.grid(row=1,column=2)
-urlEntry = Entry(root, textvariable=link)
+urlEntry = Entry(root, textvariable=link, width=25)
 urlEntry.grid(row=2, column=2)
 
-submitBtn = Button(root, text="Process", command=downloadFunc)
-submitBtn.grid(row=3, column=2, pady=(20,0))
+submitBtn = Button(root, text="Download", command=downloadFunc)
+submitBtn.grid(row=3, column=2, pady=(30,0))
 
 choose = Button(root, text="Choose Location", command=Location)
-choose.grid(row=4, column=2)
+choose.grid(row=4, column=2, pady=(30, 0))
 
-author = Label(root, text="Made by Hayden Bradford", font=('Helvetica', 10))
-author.grid(row=5, column=0, columnspan=1, pady=(75,0), padx=(10,0))
+author = Label(root, text="Made by Hayden Bradford", font=('Helvetica', 7))
+author.grid(row=5, column=2, pady=(100,0), padx=(10,0))
 
 
 root.update_idletasks()
